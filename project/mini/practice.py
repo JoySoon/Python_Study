64747bbc0f6dee2d216d25627a23fdce9d5bf26c
JoySoon <115054565+JoySoon@users.noreply.github.com>
2023-03-27 17:42:37 +0900

    var

--- project/mini/practice.py
+++ project/mini/practice.py
@@ -365,6 +365,7 @@ elif choice == "데이터페이지":
                     variable1 = np.array([승리수, 경기수])
                     model1 = joblib.load('project/XGBoost.pkl')
                     pred1 = [0]
+                    st.write(variable1)
                     # pred1 = model1.predict([variable1])
                     # pred1 = pred1.round(2)
                     st.metric("결과: ", pred1[0])
