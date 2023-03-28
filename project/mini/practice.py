143fd51902cd3c4981d943d107fd8a916975a7d5
JoySoon <115054565+JoySoon@users.noreply.github.com>
2023-03-28 18:24:16 +0900

    feature importance

--- project/mini/practice.py
+++ project/mini/practice.py
@@ -365,9 +365,9 @@ elif choice == "데이터페이지":
             fig = px.bar(
                 x=df.columns[:-1], 
                 y=model.feature_importances_, 
-                labels={'x': 'Feature', 'y': 'Importance'}
+                labels={'x': '변수', 'y': '중요도'}
                 )
-            fig.update_layout(title="변수의 중요성", xaxis_title="변수", yaxis_title="중요성")
+            fig.update_layout(title="중요 변수 확인", xaxis_title="변수", yaxis_title="중요도")
 
             st.plotly_chart(fig)
 
